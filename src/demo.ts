1256df013fded1994fdccce14a8293e96de8aa13
Igor <for_my_files@ukr.net>
2024-01-08 17:25:35 -0500

    Defining types using type aliases

--- src/demo.ts
+++ src/demo.ts
@@ -1,19 +1,12 @@
-interface Contact extends Address{
+interface Contact {
     id: number;
-    name: string;
+    name: contactName;
     birthday?: Date; // is is optional because of "?"
 }
 
-// it warns because it needs we add missing properties from Address interface
 let primaryContact: Contact = {
     id: 1,
     name: "John"
 }
 
-interface Address {
-    line1: string;
-    line2: string;
-    postalCode: string;
-    province: string;
-    region: string;
-}
+type contactName = string;
