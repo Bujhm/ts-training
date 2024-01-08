dd448abad71f3f6269313dd255d7ee21374ee9f8
Igor <for_my_files@ukr.net>
2024-01-08 16:53:23 -0500

    primitives and built in types

---
+++ src/demo.ts
@@ -0,0 +1,15 @@
+let s; //Todo: here is the warning, because we do not = it to any of types 's' or 4, etc.
+let t: number;
+let u: string;
+let v: boolean;
+let w: Date;
+let x: string[];
+let y: { name: string, age: number };
+let z: any;
+
+x = "hello" as any
+b = 1234
+
+z = 5
+z = "hello"
+z = [0, 1, 2, 3]
