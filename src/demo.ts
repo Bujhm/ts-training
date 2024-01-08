1b18febad89118866c3b0660da35a213c4ec5515
Igor <for_my_files@ukr.net>
2024-01-08 17:40:37 -0500

    defining enumerable types

--- src/demo.ts
+++ src/demo.ts
@@ -2,11 +2,19 @@ interface Contact {
     id: number;
     name: contactName;
     birthday?: Date; // is is optional because of "?"
+    status: contactStatus;
+}
+
+type contactName = string;
+
+enum contactStatus {
+    Active, //Todo: here will be 0 if we'll use it, just rename it to "active"
+    Inactive = "inactive",
+    New = "new"
 }
 
 let primaryContact: Contact = {
     id: 1,
-    name: "John"
+    name: "John",
+    status: contactStatus.Inactive
 }
-
-type contactName = string;
