cede96c0a03b3a73118cdb9ceb454fd1145f55e0
Igor <for_my_files@ukr.net>
2024-01-09 12:26:12 -0500

    typing functions

--- src/demo.ts
+++ src/demo.ts
@@ -2,19 +2,26 @@ interface Contact {
     id: number;
     name: contactName;
     birthday?: Date; // is is optional because of "?"
-    status: contactStatus;
+    clone(name:string): Contact;
 }
 
 type contactName = string;
 
-enum contactStatus {
-    Active, //Todo: here will be 0 if we'll use it, just rename it to "active"
-    Inactive = "inactive",
-    New = "new"
+//here we typed function (argument typed as Contact ans Return typed as Contact as well)
+function clone(source: Contact): Contact {
+    return Object.apply({}, source);
 }
 
-let primaryContact: Contact = {
-    id: 1,
-    name: "John",
-    status: contactStatus.Inactive
-}
+const a: Contact = { id: 123, name: "Homer Simpson", clone(name:string): Contact {
+     return { 
+        id: this.id, 
+        name: name, 
+        clone: (name) => this   //arrow function can access ->this keyword
+        } 
+    } };
+const b = clone(a);
+console.log(b);
+
+const c = a.clone("Bart Simpson");
+console.log(`this is c`, c);
+console.log(a);
