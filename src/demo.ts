37dbc43d18b17ef70e564d86384917d047884fa7
Igor <for_my_files@ukr.net>
2024-01-11 12:16:01 -0500

    typeof operator, part 2 - after

--- src/demo.ts
+++ src/demo.ts
@@ -1,3 +1,5 @@
+
+
 type contactName = string;
 type ContactStatus = "active" | "inactive" | "new"
 type contactBirthDate = Date | number | string
@@ -15,8 +17,9 @@ let primaryContact: Contact = {
     status: "active"
 }
 
-function toContact(nameOrContact) {
-    if (nameOrContact === "object") {
+// here we use the type assertion operator with pipeline operator, see the autosuggestion when using this function below, toContact(...
+function toContact(nameOrContact: string | Contact): Contact {
+    if (typeof nameOrContact === "object") {
         return {
             id: nameOrContact.id,
             name: nameOrContact.name,
@@ -34,3 +37,12 @@ function toContact(nameOrContact) {
 
 console.log(toContact(primaryContact));
 console.log(toContact("Some name"));
+
+const myType = {min: 1, max: 20}
+
+// here we use typeof operator to get the type of the object
+function saveRange(source: typeof myType) {}
+
+saveRange(primaryContact) // here will be an error
+saveRange(myType) 
+saveRange({min:-50, max:50}) 
