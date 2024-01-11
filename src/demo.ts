ff84c6c85bf15b2759042b409f9241c542b60fcd
Igor <for_my_files@ukr.net>
2024-01-11 18:36:02 -0500

    defining dynamic but limited types with records, part 2 - after, but still have issue

--- src/demo.ts
+++ src/demo.ts
@@ -1,9 +1,16 @@
-let x = { name: "Wruce Bayne" };
-x.id = 1234;
+let x: any = { name: "Wruce Bayne" }; // this is not a good practice because it allow these 👇:
+// x.id = 1234;
+// x = "test";
+// x.name = "test";
+// x.bool = true;
+// x = () => console.log("test");
 
-
-
-////////////////////
+let y: Record<string, string | number | boolean | Function> = { name: "Wruce Bayne" }; // these are much better):
+y.id = 1234;
+y.name = "test";
+y.bool = true;
+y.log = () => console.log("test");
+//y = []; // this is not allowed, because it's not a Record
 
 type ContactStatus = "active" | "inactive" | "new";
 
@@ -25,9 +32,10 @@ interface Query {
     matches(val): boolean;
 }
 
-function searchContacts(contacts: Contact[], query) {
+// this is a function that takes an array of contacts and a query object and returns a filtered array of contacts
+function searchContacts(contacts: Contact[], query: Record<keyof Contact, Query>) {
     return contacts.filter(contact => {
-        for (const property of Object.keys(contact)) {
+        for (const property of Object.keys(contact) as (keyof Contact)[]) { // here is the magic, strict type checking
             // get the query object for this property
             const propertyQuery = query[property];
             // check to see if it matches
@@ -45,6 +53,6 @@ const filteredContacts = searchContacts(
     {
         id: { matches: (id) => id === 123 },
         name: { matches: (name) => name === "Carol Weaver" },
-        phoneNumber: { matches: (name) => name === "Carol Weaver" },
+        // phoneNumber: { matches: (name) => name === "Carol Weaver" },
     }
 );
