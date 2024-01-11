338a0b0d998403ed0898c72b7cdf0d3e8b44f223
Igor <for_my_files@ukr.net>
2024-01-11 02:03:09 -0500

    keyof operator, part 1

--- src/demo.ts
+++ src/demo.ts
@@ -1,52 +1,26 @@
 type contactName = string;
-
-//key note 1: or we can use union type
+type ContactStatus = "active" | "inactive" | "new"
 type contactBirthDate = Date | number | string
 
-enum ContactStatus {
-    Active = "active",
-    Inactive = "inactive",
-    New = "new"
-}
-//key note 2: or we can use union type, shorter and better than "enum" type
-type ContactStatus2 = "active" | "inactive" | "new"
-
-interface Contact extends Address {  //key note 3: Here we adding Contact to Address
+interface Contact { 
     id: number;
     name: contactName;
     birthDate?: contactBirthDate;
-    //status: ContactStatus; //key note 4, this is also possible and workable with the "enum" type
-    status: ContactStatus2;
-}
-
-interface Address {
-    line1?: string;
-    line2?: string;
-    province?: string;
-    region: string;
-    postalCode?: string;
-}
-
-//key note 5: Here we adding Contact to Address
-interface AddressContact extends Contact, Address {} 
-//key note 6: or we can use intersection type
-type AddressContact2 = Contact & Address
-
-function getBirthDate(contact: Contact) {
-    if (typeof contact.birthDate === "number") {
-        return new Date(contact.birthDate);
-    }
-    else if (typeof contact.birthDate === "string") {
-        return Date.parse(contact.birthDate)
-    }
-    else {
-        return contact.birthDate
-    }
+    status: ContactStatus;
+    email?: string;
 }
 
 let primaryContact: Contact = {
     id: 1,
     name: "John Doe",
-    region: "New York",
-    status:"active" //key note 7: Here we can use ContactStatus2 directly, auto suggestion will work
+    status: "active"
+}
+
+type ContactFields = keyof Contact;
+
+// keyof will made constraint for ContactFileds and this will be suggesting you "id, birthDate, name, status, email" as possible values
+const field: ContactFields = "email" 
+
+function getValue(source, propertyName) {
+    return source[propertyName]
 }
