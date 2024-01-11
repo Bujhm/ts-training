2773f8c4a66fd0681892d9bb09fbf696911a10c9
Igor <for_my_files@ukr.net>
2024-01-11 11:51:47 -0500

    typeof operator, part 1 - before

--- src/demo.ts
+++ src/demo.ts
@@ -6,8 +6,7 @@ interface Contact {
     id: number;
     name: contactName;
     birthDate?: contactBirthDate;
-    status: ContactStatus;
-    email?: string;
+    status?: ContactStatus;
 }
 
 let primaryContact: Contact = {
@@ -16,31 +15,22 @@ let primaryContact: Contact = {
     status: "active"
 }
 
-type ContactFields = keyof Contact;
-
-// keyof will made constraint for ContactFileds and this will be suggesting you "id, birthDate, name, status, email" as possible values
-const field: ContactFields = "email" 
-
-// this is wrong version of getValue function
-// function getValue(source, propertyName) {
-//     return source[propertyName]
-// }
-
-// this is right version of getValue function but with Generics typing
-function getValue<T>(source: T, propertyName: keyof T) {
-    return source[propertyName]
+function toContact(nameOrContact) {
+    if (nameOrContact === "object") {
+        return {
+            id: nameOrContact.id,
+            name: nameOrContact.name,
+            status: nameOrContact.status
+        }
+    }
+    else {
+        return {
+            id: 0,
+            name: nameOrContact,
+            status: "active"
+        }
+    }
 }
 
-// this is the copy of previous function, but more generic using 2 generics
-function getValue2<T, U extends keyof T>(source: T, propertyName: U) {
-    return source[propertyName]
-}
-
-const value = getValue(primaryContact, "status"); // now this works correctly
-console.log(value);
-
-const value2 = getValue({min:1, max:200}, "max"); // after using Generic typing this will work also! 
-console.log(value2);
-
-const value3 = getValue2(primaryContact, "name"); // example of getValue2() function with 2 generics
-console.log(value3);
+console.log(toContact(primaryContact));
+console.log(toContact("Some name"));
