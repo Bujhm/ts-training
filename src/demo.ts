9b18a4a17a69b4fc003969ca4994f4d74a46fd95
Igor <for_my_files@ukr.net>
2024-01-11 16:08:49 -0500

    defining dynamic but limited types with records, part 1 - before

--- src/demo.ts
+++ src/demo.ts
@@ -1,3 +1,10 @@
+let x = { name: "Wruce Bayne" };
+x.id = 1234;
+
+
+
+////////////////////
+
 type ContactStatus = "active" | "inactive" | "new";
 
 interface Address {
@@ -13,41 +20,31 @@ interface Contact {
     address: Address;
 }
 
-interface ContactEvent {
-    contactId: Contact["id"]; // here is indexed typing
+interface Query {
+    sort?: 'asc' | 'desc';
+    matches(val): boolean;
 }
 
-interface ContactDeletedEvent extends ContactEvent { 
+function searchContacts(contacts: Contact[], query) {
+    return contacts.filter(contact => {
+        for (const property of Object.keys(contact)) {
+            // get the query object for this property
+            const propertyQuery = query[property];
+            // check to see if it matches
+            if (propertyQuery && propertyQuery.matches(contact[property])) {
+                return true;
+            }
+        }
+
+        return false;
+    })
 }
 
-interface ContactStatusChangedEvent extends ContactEvent { 
-    oldStatus: Contact["status"]; // here
-    newStatus: Contact["status"]; // and here
-}
-
-interface ContactEvents {
-    deleted: ContactDeletedEvent;
-    statusChanged: ContactStatusChangedEvent;
-    // ... and so on
-}
-
-function getValue<T, U extends keyof T>(source: T, propertyName: U) {
-    return source[propertyName];
-}
-
-function handleEvent<T extends keyof ContactEvents>(
-    eventName: T,
-    handler: (evt: ContactEvents[T]) => void | object
-) {
-    if (eventName === "statusChanged") {
-        handler({ contactId: 1, oldStatus: "active", newStatus: "inactive" })
+const filteredContacts = searchContacts(
+    [/* contacts */],
+    {
+        id: { matches: (id) => id === 123 },
+        name: { matches: (name) => name === "Carol Weaver" },
+        phoneNumber: { matches: (name) => name === "Carol Weaver" },
     }
-    // if (eventName === "deleted") {
-    //    handler({contactId: 1 } as ContactDeletedEvent) // here willbe an error, because it should implement 2 interfaces
-    //}
-    // if (eventName === "deleted") {
-    //     handler({ contactId: 1, oldStatus: "active", newStatus: "inactive"})  // here everything will be OK
-    // }
-}
-
-handleEvent("statusChanged", evt => evt)
+);
