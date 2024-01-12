a7129b0ca96112dcf2b4f7e07703028bd17d95f2
Igor <for_my_files@ukr.net>
2024-01-11 21:06:32 -0500

    extracting metadata from existing types, part 2, after

--- src/demo.ts
+++ src/demo.ts
@@ -14,19 +14,24 @@ interface Contact {
     email: string;
 }
 
-interface Query {
+interface Query<TProp> {
     sort?: 'asc' | 'desc';
-    matches(val): boolean;
+    matches(val: TProp): boolean;
 }
 
- type ContactQuery = Partial<Record<keyof Contact, Query>>
+//  type ContactQuery = Partial<Record<keyof Contact, Query>>
+
+// this is the same as the above, but with a little more code and more readable 
+type ContactQuery = {
+    [TProp in keyof Contact]?: Query<Contact[TProp]>  // that adopts to each target property, index satic type
+}
 
 // this is a function that takes an array of contacts and a query object and returns a filtered array of contacts
 function searchContacts(contacts: Contact[], query: ContactQuery ) {
     return contacts.filter(contact => {
         for (const property of Object.keys(contact) as (keyof Contact)[]) { // here is the magic, strict type checking
             // get the query object for this property
-            const propertyQuery = query[property];
+            const propertyQuery = query[property] as Query<Contact[keyof Contact]>; // the as is a little bit of a hack, but it works
             // check to see if it matches
             if (propertyQuery && propertyQuery.matches(contact[property])) {
                 return true;
